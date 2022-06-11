a7117f3302e74efddb7cdcf6152abfc16bffdd96
fuadsuleyman <fuadsuleymanov@outlook.com>
2022-06-11 11:36:00 +0400

    create App.vue and ResourceItem component and show in screen via props

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <resource-item-vue v-for="res in storedResources" :key="res.id" :title="res.title" :link="res.link" :description="res.description"></resource-item-vue>
+  </ul>
+</template>
+
+<script>
+import ResourceItemVue from './components/learning-resources/ResourceItem.vue';
+export default {
+  components: { ResourceItemVue },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official guide',
+          title: 'Official Vue Guide',
+          description: 'to learn knowledge from first hand is so important',
+          link: 'https://vuejs.org/',
+        },
+        {
+          id: 'search materials',
+          title: 'Google search',
+          description: 'find materials from different resources about vue',
+          link: 'https://google.com/',
+        },
+      ],
+    };
+  },
+};
+</script>
