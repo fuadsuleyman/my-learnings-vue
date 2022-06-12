ac886e368f77d6d8cdc5109dd784b473edef5c14
fuadsuleyman <fuadsuleymanov@outlook.com>
2022-06-12 15:35:22 +0400

    create BaseDialog component but not use yet

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import App from './App.vue';
 
 import BaseCard from '../src/components/UI/BaseCard';
 import BaseButton from '../src/components/UI/BaseButton';
+import BaseDialog from '../src/components/UI/BaseDialog';
 
 const app = createApp(App);
 
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
+app.component('base-dialog', BaseDialog);
 
 app.mount('#app');
